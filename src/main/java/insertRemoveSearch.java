be179026c8768ea5928288505ad6b27d2ec9f56e
Sander Skau Karlsen <sanderkar@stud.ntnu.no>
2026-09-11 10:51:44 +0200

    Første del

---
+++ src/main/java/insertRemoveSearch.java
@@ -0,0 +1,56 @@
+public class insertRemoveSearch {
+
+  public void insert(int item, int index){
+    /**
+     * Implement the insert function, which inserts the given item at the given position.
+     * If the underlying array is full, it shall resize the array appropriately, by doubling its capacity.
+     */
+
+    // TODO: Implement
+    throw new RuntimeException("Not implemented yet");
+  }
+
+
+  public void remove(int index){
+    /**
+     * Implement the remove function, which delete the item stored at the given index.
+     * If the underlying array gets empty, it shall resize the array appropriately.
+     * It should halve the capacity as soon as the load is less or equal than 25 %.
+     */
+    // TODO: Implement
+    throw new RuntimeException("Not implemented yet");
+  }
+
+  /**
+   *Implement the search function, which returns an index where the given item can be found,
+   * or 0 if the sequence does not include that item.
+   */
+  public int search(int item){
+    // TODO: Implement
+    throw new RuntimeException("Not implemented yet");
+  }
+
+
+  // Extrema
+
+  /**
+   * Propose an algorithm the finds both the minimum and the maximum of the sequence.
+   */
+  public int[] extrema(){
+    // TODO: Implement
+    throw new RuntimeException("Not implemented yet");
+  }
+}
+
+/**
+ * What is the worst-case scenario for your algorithm?
+ * Give an sample sequence that triggers that worst case.
+ */
+
+/**
+ * Given a sequence of length
+ * ℓ
+ * , how many comparisons are needed in the worst case. Express it as a function of
+ * ℓ
+ * .
+ */
