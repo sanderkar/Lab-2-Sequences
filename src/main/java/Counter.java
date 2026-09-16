61146b632cdc366cb5a5fa34a91efd8c7f951356
Matias <matiashh@live.no>
2026-09-16 18:08:40 +0200

    + Counter

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,2 +1,13 @@
 public class Counter {
+
+    private DigitDisplay digits[];
+
+    public void increment() {
+        // TODO: Implement, by calling digits[i].next() when appropriate
+    }
+
+}
+
+class DigitDisplay {
+    // ...
 }
